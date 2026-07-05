d83e96cfb5ade291b056f106658aef876ee851f1
sdz <sdz@sdz-mods.com>
2026-07-05 03:47:38 +0300

    mb/dell/precision_m4800_3dfx: Add mSATA controller setup option
    Define the "sata2" CMOS option (bit 423, default Enable) consumed by
    the southbridge, and gate the romstage function-disable of 00:1f.5 on
    it.

--- src/mainboard/dell/precision_m4800_3dfx/romstage.c
+++ src/mainboard/dell/precision_m4800_3dfx/romstage.c
@@ -4,13 +4,24 @@
 #include <device/pci_def.h>
 #include <device/pci_ids.h>
 #include <device/pci_ops.h>
+#include <option.h>
 #include <southbridge/intel/lynxpoint/pch.h>
 
 void mainboard_config_rcba(void)
 {
-	RCBA32_AND_OR(FD, ~PCH_DISABLE_SATA2, 0);
-	if (pci_read_config16(PCH_SATA_DEV, PCI_DEVICE_ID) == PCI_DID_INTEL_LPT_H_MOBILE_SATA_IDE)
-		pci_or_config16(PCH_SATA_DEV, SATA_PCS, 1 << 9);
+	/*
+	 * The second SATA function (00:1f.5, mSATA in IDE native mode) can
+	 * be hidden from the "sata2" setup option, e.g. to present old OSes
+	 * with a single IDE controller.
+	 */
+	if (get_uint_option("sata2", 1)) {
+		RCBA32_AND_OR(FD, ~PCH_DISABLE_SATA2, 0);
+		if (pci_read_config16(PCH_SATA_DEV, PCI_DEVICE_ID) ==
+		    PCI_DID_INTEL_LPT_H_MOBILE_SATA_IDE)
+			pci_or_config16(PCH_SATA_DEV, SATA_PCS, 1 << 9);
+	} else {
+		RCBA32_OR(FD, PCH_DISABLE_SATA2);
+	}
 
 	RCBA16(D20IR) = DIR_ROUTE(PIRQA, PIRQB, PIRQC, PIRQD);
 	RCBA16(D25IR) = DIR_ROUTE(PIRQE, PIRQF, PIRQG, PIRQH);
